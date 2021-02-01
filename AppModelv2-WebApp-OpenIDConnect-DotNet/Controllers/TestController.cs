2ef8e21b3b567a1a74b52b89bc00ef677e829137
Jegan Matthews <jematthe@microsoft.com>
2021-01-31 23:25:32 -0800

    updated authentication

---
+++ AppModelv2-WebApp-OpenIDConnect-DotNet/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppModelv2_WebApp_OpenIDConnect_DotNet.Controllers
+{
+	[Authorize]
+	public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		[AllowAnonymous]
+		public ActionResult OpenRoute()
+		{
+			return View();
+		}
+	}
+}
--- auth-x/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="auth_x.MvcApplication" Language="C#" %>
